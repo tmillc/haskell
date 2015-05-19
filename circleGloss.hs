8bc525e390ea03fe5ac30e8074fe0b6dda62c16c
tmillc <dodecalogue@gmail.com>
2015-05-18 20:31:51 -0500

    got rid of circleGloss, don't understand it at all, it was from reactive-banana anti-tutorial

--- circleGloss.hs
+++
@@ -1,7 +0,0 @@
-import Graphics.Gloss
-
-main :: IO ()
-main = playBanana (InWindow "Nice Window" (200,200) (800,200))
-                    white
-                    30
-                    (\_ _ -> return $ pure $ circle 10)
--- helloworld-wx.hs
+++ helloworld-wx.hs
@@ -9,6 +9,8 @@ hello :: IO ()
 hello
   = do  f       <- frame    [text := "Hello!"]
         quit    <- button f [text := "Quit", on command := close f]
+        thing   <- button f [text := "Nope"]
         set f [layout := margin 10 (column 5 [floatCentre (label "Hello")
                                              ,floatCentre (widget quit)
+                                             ,floatCentre (widget thing)
                                              ] ) ]
